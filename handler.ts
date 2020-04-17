f7b126b1586dac5a05452f56715a919dc8a5e110
Sylvain Millot <sylvain@hastatis.com>
2020-04-17 08:16:04 +0200

    API test

--- handler.ts
+++ handler.ts
@@ -1,5 +1,23 @@
-export const parse = (event: { body: string }) => {
-  const data: { body: string } = JSON.parse(event.body);
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
 
-  console.log(data.body);
-};
+/**
+ * This function get an email body in params, and return a json object like this :
+ * {
+ *  phone: XXX,
+ *  mobile: XXX,
+ *  website: XXX
+ * }
+ */
+app.get("/parse", function (req, res) {
+  // Get params
+
+  // Use regex to find what we want
+
+  // Return result
+
+  res.send("Hello World!");
+});
+
+module.exports.handler = serverless(app);
