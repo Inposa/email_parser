f79a4619147582a00ec774d2bc60c8483ed04d93
Inposa <donhippo04@gmail.com>
2020-04-22 11:09:35 +0200

    Get param in post body and return json phone number & website

--- handler.ts
+++ handler.ts
@@ -2,6 +2,10 @@ const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 /**
  * This function get an email body in params, and return a json object like this :
  * {
@@ -10,14 +14,22 @@ const app = express();
  *  website: XXX
  * }
  */
-app.get("/parse", function (req, res) {
+app.post("/parse", function (req, res) {
   // Get params
+  const queryMessage = req.body.emailBody;
+  const bodyString = JSON.stringify(queryMessage);
+
+  const phoneNumbers = bodyString.match(/((0|\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9]))[1-9]([- .]?[0-9]{2}){4})/g)
+  
+  const websites = bodyString.match(/\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/g)
 
   // Use regex to find what we want
 
   // Return result
+  res.send({
+    phones: phoneNumbers,
+    websites: websites,
+  })
 
-  res.send("Hello World!");
 });
-
 module.exports.handler = serverless(app);
