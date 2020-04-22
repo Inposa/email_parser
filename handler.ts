d03428389f5525f68f34fedf69b829493aa6958d
Sylvain Millot <sylvain@hastatis.com>
2020-04-22 14:28:41 +0200

    prettier

--- handler.ts
+++ handler.ts
@@ -2,7 +2,7 @@ const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -20,14 +20,17 @@ app.post("/parse", function (req, res) {
   const bodyString = JSON.stringify(queryMessage);
 
   // Use regex to find what we want
-  const phoneNumbers = bodyString.match(/((0|\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9]))[1-9]([- .]?[0-9]{2}){4})/g)
-  const websites = bodyString.match(/\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/g)
+  const phoneNumbers = bodyString.match(
+    /((0|\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9]))[1-9]([- .]?[0-9]{2}){4})/g
+  );
+  const websites = bodyString.match(
+    /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/g
+  );
 
   // Return result
   res.send({
     phones: phoneNumbers,
     websites: websites,
-  })
-
+  });
 });
 module.exports.handler = serverless(app);
