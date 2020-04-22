8089ce356dd14a4caea9698ece406f6b75b62b28
Inposa <donhippo04@gmail.com>
2020-04-22 11:13:40 +0200

    Agencement des commentaires

--- handler.ts
+++ handler.ts
@@ -19,11 +19,9 @@ app.post("/parse", function (req, res) {
   const queryMessage = req.body.emailBody;
   const bodyString = JSON.stringify(queryMessage);
 
-  const phoneNumbers = bodyString.match(/((0|\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9]))[1-9]([- .]?[0-9]{2}){4})/g)
-  
-  const websites = bodyString.match(/\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/g)
-
   // Use regex to find what we want
+  const phoneNumbers = bodyString.match(/((0|\+?([1-9]|[0-9][0-9]|[0-9][0-9][0-9]))[1-9]([- .]?[0-9]{2}){4})/g)
+  const websites = bodyString.match(/\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/g)
 
   // Return result
   res.send({
